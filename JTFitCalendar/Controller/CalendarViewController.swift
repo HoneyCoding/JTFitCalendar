1bc091a3485bcb58f260fe342e49ea1006eef2a4
HoneyCoding <dd3557@naver.com>
2024-04-13 16:49:42 +0900

    Feat: CalendarViewController에 달력이 나타나는 기능 추가

--- JTFitCalendar/Controller/CalendarViewController.swift
+++ JTFitCalendar/Controller/CalendarViewController.swift
@@ -5,8 +5,58 @@
 //  Created by 김진태 on 4/13/24.
 //
 
+import FSCalendar
+import SnapKit
+import Then
 import UIKit
 
 class CalendarViewController: UIViewController {
+	private lazy var calendarView: FSCalendar = FSCalendar().then {
+		$0.scope = .month
+		$0.locale = Locale(identifier: "ko_KR")
+		$0.scrollEnabled = true
+		$0.scrollDirection = .horizontal
+		$0.weekdayHeight = 36
+		$0.headerHeight = 0
+		$0.delegate = self
+	}
 	
+	private let navigationBarTitleDateFormatter = DateFormatter().then {
+		$0.dateFormat = "yyyy년 MM월"
+		$0.locale = Locale(identifier: "ko_kr")
+		$0.timeZone = TimeZone(identifier: "KST")
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setupViews()
+	}
+	
+	private func setupViews() {
+		setupCalendarView()
+		setupNavigationBar()
+	}
+	
+	private func setupCalendarView() {
+		view.addSubview(calendarView)
+		calendarView.snp.makeConstraints { make in
+			make.top.equalTo(view.safeAreaLayoutGuide)
+			make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+			make.height.equalTo(300)
+		}
+		
+		calendarView.select(Date.now)
+	}
+	
+	private func setupNavigationBar() {
+		navigationItem.title = navigationBarTitleDateFormatter.string(from: calendarView.currentPage)
+	}
+}
+
+extension CalendarViewController: FSCalendarDelegate {
+	func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+		let currentPage = calendar.currentPage
+		
+		navigationItem.title = navigationBarTitleDateFormatter.string(from: currentPage)
+	}
 }
