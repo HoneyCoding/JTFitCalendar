975b1baaf36eac29d34a8842de2c4c3286ae24a0
HoneyCoding <dd3557@naver.com>
2024-04-13 21:32:20 +0900

    Feat: AddFitItemViewController 클래스 추가

---
+++ JTFitCalendar/Controller/AddFitItemViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddFitItemViewController.swift
+//  JTFitCalendar
+//
+//  Created by 김진태 on 4/13/24.
+//
+
+import UIKit
+
+class AddFitItemViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = UIColor.systemBackground
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		navigationController?.isNavigationBarHidden = false
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		navigationController?.isNavigationBarHidden = true
+	}
+}
