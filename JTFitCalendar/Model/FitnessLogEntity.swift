2c473d4def4012a1bea1ad2dd5c1dc4bf49b048a
HoneyCoding <dd3557@naver.com>
2024-04-14 09:03:57 +0900

    Feat: CoreData 모델 추가

---
+++ JTFitCalendar/Model/FitnessLogEntity.swift
@@ -0,0 +1,15 @@
+//
+//  FitnessLogEntity+CoreDataClass.swift
+//  JTFitCalendar
+//
+//  Created by 김진태 on 4/14/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(FitnessLogEntity)
+public class FitnessLogEntity: NSManagedObject {
+
+}
