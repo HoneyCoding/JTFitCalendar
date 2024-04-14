234869b730086a6ad970813b50500e933f139508
HoneyCoding <dd3557@naver.com>
2024-04-14 18:58:06 +0900

    Comment: AppDelegate에 주석 추가

--- JTFitCalendar/App/AppDelegate.swift
+++ JTFitCalendar/App/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+		// IQKeyboardManager 설정
 		IQKeyboardManager.shared.enable = true
 		IQKeyboardManager.shared.enableAutoToolbar = false
 		return true
