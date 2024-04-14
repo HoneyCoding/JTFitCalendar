8e6cd03a52068669dd8b343cc6a0138314b624de
HoneyCoding <dd3557@naver.com>
2024-04-14 13:05:48 +0900

    Comment: MainTabBarController에 주석 작성

--- JTFitCalendar/Controller/MainTabBarController.swift
+++ JTFitCalendar/Controller/MainTabBarController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
+
+/// 앱을 실행하면 제일 먼저 나타나는 Tab Bar 화면을 구현한 Controller이다.
 class MainTabBarController: UITabBarController {
 
+	// MARK: - View Lifecycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		view.backgroundColor = UIColor.systemBackground
@@ -16,6 +19,7 @@ class MainTabBarController: UITabBarController {
 		setupTabBar()
 	}
 	
+	/// Tab Bar를 통해 전환할 수 있는 3개의 컨트롤러를 설정해준다.
 	private func setupViewControllers() {
 		let firstViewController = createNavigationController(
 			image: UIImage(systemName: "calendar"), 
@@ -41,6 +45,7 @@ class MainTabBarController: UITabBarController {
 		)
 	}
 	
+	/// Tab Bar의 디자인을 수정하는 함수이다. 해당 함수가 실행되면 TabBar는 System Background와 같은 배경색을 갖게 된다.
 	private func setupTabBar() {
 		let appearance = UITabBarAppearance()
 		appearance.configureWithOpaqueBackground()
@@ -50,6 +55,12 @@ class MainTabBarController: UITabBarController {
 		tabBar.scrollEdgeAppearance = appearance
 	}
 	
+	/// 해당 함수를 통해 tabBarItem이 설정된 NavigationController를 생성한다.
+	/// - Parameters:
+	///   - image: tabBarItem의 image로 사용된다.
+	///   - title: tabBarItem의 title로 사용된다.
+	///   - rootViewController: UINavigationController로 감싸주는 viewController이다.
+	/// - Returns: rootViewController를 UINavigationController로 감싸주어 return한다.
 	private func createNavigationController(
 		image: UIImage?, title: String?, rootViewController: UIViewController
 	) -> UINavigationController {
