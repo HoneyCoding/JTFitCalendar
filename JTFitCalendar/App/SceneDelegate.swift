8e3cae3549c8c050ee04393290038e2dfda0e2d4
HoneyCoding <dd3557@naver.com>
2024-04-14 18:57:27 +0900

    Comment: SceneDelegate에 주석 추가

--- JTFitCalendar/App/SceneDelegate.swift
+++ JTFitCalendar/App/SceneDelegate.swift
@@ -12,7 +12,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	var window: UIWindow?
 
 
-	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+	func scene(
+		_ scene: UIScene,
+		willConnectTo session: UISceneSession,
+		options connectionOptions: UIScene.ConnectionOptions
+	) {
+		// 윈도우 설정
 		guard let scene = (scene as? UIWindowScene) else { return }
 		let window = UIWindow(windowScene: scene)
 		window.backgroundColor = UIColor.systemBackground
