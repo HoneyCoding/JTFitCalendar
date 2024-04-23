a6361879d763a3210a1571591791ab5384b58d0e
HoneyCoding <dd3557@naver.com>
2024-04-23 21:54:55 +0900

    Feat: SettingsViewController에 tableView 추가

--- JTFitCalendar/Controller/SettingsViewController.swift
+++ JTFitCalendar/Controller/SettingsViewController.swift
@@ -5,11 +5,28 @@
 //  Created by 김진태 on 4/13/24.
 //
 
+import SnapKit
+import Then
 import UIKit
 
 class SettingsViewController: UIViewController {
+	
+	private let tableView: UITableView = UITableView(frame: .zero, style: .plain)
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		setupViews()
+	}
+	
+	private func setupViews() {
 		view.backgroundColor = UIColor.jtBackgroundColor
+		setupTableView()
+	}
+	
+	private func setupTableView() {
+		view.addSubview(tableView)
+		tableView.snp.makeConstraints { make in
+			make.edges.equalToSuperview()
+		}
 	}
 }
