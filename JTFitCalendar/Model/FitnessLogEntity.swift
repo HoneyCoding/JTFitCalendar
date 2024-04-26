256248e49cb1ae332332c7853451f8e49c35f653
HoneyCoding <dd3557@naver.com>
2024-04-26 11:45:13 +0900

    Comment: FitnessLogEntity에 대한 주석 추가

--- JTFitCalendar/Model/FitnessLogEntity.swift
+++ JTFitCalendar/Model/FitnessLogEntity.swift
@@ -12,15 +12,21 @@ import UIKit
 
 @objc(FitnessLogEntity)
 public class FitnessLogEntity: NSManagedObject {
+	
+	/// imageData를 변환하여 image를 생성하거나 image를 imageData로 변환하는 계산 프로퍼티이다.
 	var image: UIImage? {
 		get {
+			// imageData를 변환하여 image를 생성
 			guard let imageData else { return nil }
 			return UIImage(data: imageData)
 		} set {
+			// image를 imageData로 변환
 			self.imageData = newValue?.pngData()
 		}
 	}
 	
+	
+	/// FitnessLogEntity의 hour, minutes, seconds를 합쳐 텍스트로 변환해주는 프로퍼티이다.
 	var activityTimeText: String? {
 		var activityTimeText = ""
 		
