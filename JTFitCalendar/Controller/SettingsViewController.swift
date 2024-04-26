7960ec6f542f09e50271cd29326fe716f52a3976
HoneyCoding <dd3557@naver.com>
2024-04-26 18:34:51 +0900

    Comment: SettingsViewController에 대한 주석 추가

--- JTFitCalendar/Controller/SettingsViewController.swift
+++ JTFitCalendar/Controller/SettingsViewController.swift
@@ -9,20 +9,26 @@ import SnapKit
 import Then
 import UIKit
 
+// Tab Bar의 세 번째 화면인 Settings 화면에 대한 ViewController이다.
 class SettingsViewController: UIViewController {
 	
+	// MARK: - View Properties
+	// 화면 전체를 덮는 TableView이다.
 	private let tableView: UITableView = UITableView(frame: .zero, style: .plain)
 	
+	// MARK: - View Lifecycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		setupViews()
 	}
 	
+	/// View에 대한 Constraint 및 속성을 설정하는 함수이다.
 	private func setupViews() {
 		view.backgroundColor = UIColor.jtBackgroundColor
 		setupTableView()
 	}
 	
+	/// TableView에 대한 Constraint 설정을 하는 함수이다.
 	private func setupTableView() {
 		view.addSubview(tableView)
 		tableView.snp.makeConstraints { make in
